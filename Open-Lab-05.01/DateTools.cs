6089b3a84184fa0b4426e6581c11fadc430464c6
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-11-05 15:44:23 +0100

    I implemented method that takes a year and returns a formatted string with the right century.

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,8 +6,47 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            var storocie = Century(year / 100) + 1;
-            return storocie;
+            if (year % 100 == 0) //1800
+            {
+                int new_century = year / 100; //18
+                if (new_century.ToString().EndsWith("1") && new_century != 11)
+                {
+                    return new_century + "st century";
+                }
+                else if (new_century.ToString().EndsWith("2") && new_century != 12)
+                {
+                    return new_century + "nd century";
+                }
+                else if (new_century.ToString().EndsWith("3") && new_century != 13)
+                {
+                    return new_century + "rd century";
+                }
+                else
+                {
+                    return new_century + "th century";
+                }
+            }
+            else
+            {
+                double new_century = ((year / 100 + 1));//18,89
+                var final_century = Math.Floor(new_century);
+                if (final_century.ToString().EndsWith("1") && new_century != 11)
+                {
+                    return new_century + "st century";
+                }
+                else if (final_century.ToString().EndsWith("2") && final_century != 12)
+                {
+                    return final_century + "nd century";
+                }
+                else if (final_century.ToString().EndsWith("3") && final_century != 13)
+                {
+                    return final_century + "rd century";
+                }
+                else
+                {
+                    return final_century + "th century";
+                }
+            }
         }
     }
 }
